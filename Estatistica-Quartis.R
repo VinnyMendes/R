73a9d27f8357cc12d7c1780158f61ad59e7d68f8
Vinicius Mendes da Silva <vinicius.ms@dinfor.uniceub>
2022-09-15 10:49:24 -0300

    lista aula 15/09

--- Estatistica-Quartis.R
+++ Estatistica-Quartis.R
@@ -64,6 +64,74 @@ j = (1/(sigma*sqrt(2*pi)))*exp(-(((sequencia-mu)^2)/(2*sigma^2)))
 plot(sequencia, j)
 
 
+#prob = pnorm(b,mean = media, sd = sqrt(var)) - pnorm(a,mean = media, sd = sqrt(var))
+#a variância é o quadrado do desvio padrão
+
+#1 a
+pnorm(5, 8, 2)
+
+#1 b
+b = 1 - pnorm(9.5, 8, 2)
+pnorm(9.5, 8, 2)
+
+#1 c
+c = pnorm(10,8,2) - pnorm(7, 8, 2)
+c
+
+#1 d
+d = qnorm(0.75, 8,2) #qnorm recebe a probabilidade como argumento
+
+#2 a
+1 - (pnorm(22.86 + 0.127, 22.86,0.0762) - pnorm(22.86 - 0.127, 22.86,0.0762))
+
+#2 b
+qnorm(0.005, 22.86, 0.0762) #limite de baixo
+
+qnorm(0.995, 22.86, 0.0762) #limite de cima
+
+#3 a
+1 - pnorm(1.9, 1.8,0.1)
+
+#3 b
+qnorm(0.3, 1.8, 0.1)
+
+#4 a
+pnorm(72, 70, 4) - pnorm(62, 70,4)
+
+#4 b #valor de c tem que ser 7.83986
+qnorm(0.025, 70, 4)
+qnorm(0.975, 70, 4)
+70 - c = 62
+c <- 70 - 62.16014
+c2 <- 77.83986 - 70
+
+#5 a
+1 - pnorm(600, 500, 150) #25 %
+
+#5 b fica pra depois
+
+#6 a
+
+#7c
+#media = mediana na distribuição normal
+#distância interquartil é o 3 quartil - 1 quartil
+qnorm(0.75, 1.8, 0.1) - qnorm(0.25, 1.8, 0.1)
+
+#
+1 - pnorm(325, 345, 12)
+pnorm(325, 345, 12)
+
+(500*0.05+1200*0.95) - 200
+
+
+
+
+
+
+
+
+
+
 
 
 
